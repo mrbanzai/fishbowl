262ac69e8165b4feae34bd071284b805bd8785fc
Michael Porter <michael@skookum.com>
2013-05-30 11:51:10 -0400

    Handle errors at multiple response node levels

--- lib/fishbowl/requests/base_request.rb
+++ lib/fishbowl/requests/base_request.rb
@@ -19,14 +19,10 @@ module Fishbowl::Requests
       @ticket = response_doc.at_xpath('//Ticket')
 
       response_node = response_doc.at_xpath('//FbiMsgsRs')
-      if specific_response = response_node.at_xpath('*')
-        response_node = specific_response
+      check_response(response_node)
+      if specific_response_node = response_node.at_xpath('*')
+        check_response(specific_response_node)
       end
-
-      @status_code = response_node.attr('statusCode')
-      @status_message = response_node.attr('statusMessage')
-
-      Fishbowl::Errors.confirm_success_or_raise(@status_code, @status_message)
 
       [@status_code, @status_message, @ticket, distill(response_doc)]
     end
@@ -52,6 +48,13 @@ module Fishbowl::Requests
 
     def distill(response_doc)
       nil
+    end
+
+    def check_response(node)
+      @status_code = node.attr('statusCode')
+      @status_message = node.attr('statusMessage')
+
+      Fishbowl::Errors.confirm_success_or_raise(@status_code, @status_message)
     end
 
   end
