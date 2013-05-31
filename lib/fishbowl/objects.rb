a56070d2261fbc0612d9079f27d26ac959c36448
Michael Porter <michael@skookum.com>
2013-05-31 14:46:02 -0400

    Mixin ability to build an object from a hash
    Todo: allow passing in recursive hashes, instead of requiring object properties to be Fishbowl::Objects

--- lib/fishbowl/objects.rb
+++ lib/fishbowl/objects.rb
@@ -5,3 +5,16 @@ end
 Dir.glob(File.join(File.expand_path('../objects/', __FILE__), '**.rb')).each do |file|
   require file
 end
+
+# Since there's currently no base object class, I'll
+# mix it in here for now
+Fishbowl::Objects.constants.each do |c|
+  class_obj = Fishbowl::Objects.const_get(c)
+  class_obj.class_eval do
+    def self.from_hash(h)
+      klass = self.new
+      h.each { |k, v| klass.send("#{k}=", v) }
+      klass
+    end
+  end
+end
